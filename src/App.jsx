3d008fbab86518af4c8038506ad44f5e04f00bb8
2400080075 <2400080075@kluniversity.in>
2025-11-30 02:09:38 +0530

    fix: add basename to BrowserRouter for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/Placement-Interaction-System">
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/login" element={<Login />} />
